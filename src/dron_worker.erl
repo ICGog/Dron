20586e97a691ce363006943b0fdea23493826d7c
Ionel Gog <gogionel@gmail.com>
2011-10-03 13:49:31 +0300

    This time with the file as well:)

---
+++ src/dron_worker.erl
@@ -0,0 +1,41 @@
+-module(dron_worker).
+-author('Ionel Corneliu Gog').
+-include("dron.hrl").
+-behaviour(gen_server).
+
+-export([start_worker/0, run/1]).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+%--------------------------------------------------------------------------------
+% API
+%--------------------------------------------------------------------------------
+
+start_worker() ->
+    gen_server:start_link(?NAME, ?MODULE, [], []).
+
+run(Cmd) ->
+    gen_server:cast(?NAME, {run, Cmd}).
+
+%--------------------------------------------------------------------------------
+% Internal
+%--------------------------------------------------------------------------------
+
+init([]) ->
+    {ok}.
+
+handle_call(_Request, _From, _State) ->
+    not_implemented.
+
+handle_cast({run, Cmd}, _State) ->
+    not_implemented.
+
+handle_info(_Info, _State) ->
+    not_implemented.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
