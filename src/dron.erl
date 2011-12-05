b4cef1072538cb07b5976df815175909cb8c5f5d
Ionel Gog <gogionel@gmail.com>
2011-12-05 22:23:36 +0000

    Setup exchanges on startup.

--- src/dron.erl
+++ src/dron.erl
@@ -24,10 +24,16 @@ start(_Type, _Args) ->
     {ok, Sup} = dron_sup:start(),
     dron_mnesia:start([node()], [{n_ram_copies, 1}]),
     error_logger:info_msg("~nDron Scheduler is running!~n", []),
+    dron_pubsub:start_link(),
+    lists:map(fun({Exchange, Type}) ->
+                      dron_pubsub:setup_exchange(Exchange, Type) end,
+              dron_config:dron_exchanges()),
+    error_logger:info_msg("~nDron PubSub is running!~n", []),
     AutoWorkers = dron_pool:auto_add_workers(),
     error_logger:info_msg("Auto attaching workers: ~w~n", [AutoWorkers]),
     {ok, Sup}.
 
 stop(_State) ->
+    dron_pubsub:stop_exchange(dron_config:job_instance_exchange()),
     dron_mnesia:stop(),
     ok.
--- src/dron_config.erl
+++ src/dron_config.erl
@@ -1,9 +1,18 @@
 -module(dron_config).
 -author("Ionel Corneliu Gog").
 
--export([max_slots/0]).
+-export([max_slots/0, dron_consumer/0, dron_exchanges/0, job_instance_key/0]).
 
 %-------------------------------------------------------------------------------
 
 max_slots() ->
     20.
+
+dron_consumer() ->
+    dron_consumer.
+
+dron_exchanges() ->
+    [{<<"dron_job_instances">>, <<"direct">>}].
+
+job_instance_key() ->
+    <<"dron_job_instance">>.
