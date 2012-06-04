4e01789e02c18f0171c1f51763b2f72bd99905ff
Ionel Gog <gogionel@gmail.com>
2012-06-04 18:26:14 +0100

    Increased slot numbers

--- src/dron_config.erl
+++ src/dron_config.erl
@@ -24,7 +24,7 @@ db_nodes() ->
   end.    
 
 max_slots() ->
-  1000.
+  2000.
 
 exchanges() ->
   [{<<"dron_events">>, <<"fanout">>},
--- src/dron_worker.erl
+++ src/dron_worker.erl
@@ -172,9 +172,9 @@ run_job_instance(#job_instance{jid = JId, name = Name, cmd_line = Cmd}) ->
            {Pid, start} -> Pid
            after 10000  -> exit(no_start_timeout)
          end,
-  FileName = io_lib:format("~s_~p-~p-~p-~p:~p:~p",
-                           [Name, Y, M, D, H, Min, Sec]),
-  file:write_file(FileName, io_lib:fwrite("~s", [os:cmd(Cmd)]), [write]),
+%  FileName = io_lib:format("~s_~p-~p-~p-~p:~p:~p",
+%                           [Name, Y, M, D, H, Min, Sec]),
+%  file:write_file(FileName, io_lib:fwrite("~s", [os:cmd(Cmd)]), [write]),
   WPid ! {JId, self(), ok}.
 
 clear_timeout(JId, JITimeout) ->
